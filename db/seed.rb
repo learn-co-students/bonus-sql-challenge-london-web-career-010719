f9886dc72dc9dce90881b01c70f852645ab3ebea
YSongYS <songyishu.yang@gmail.com>
2019-01-17 10:13:45 +0000

    ahhhhh finally

--- db/seed.rb
+++ db/seed.rb
@@ -1,1 +1,31 @@
 # Parse the CSV and seed the database here! Run 'ruby db/seed' to execute this code.
+require 'sqlite3'
+require 'csv'
+require 'pry'
+#require_relative '../lib/queries.rb'
+
+sql = <<-SQL
+  CREATE TABLE guests (
+    id INTEGER PRIMARY KEY,
+    year INTEGER,
+    occupation TEXT,
+    show TEXT,
+    showgroup TEXT,
+    name TEXT
+    );
+  SQL
+DB = {:conn => SQLite3::Database.new("guests.db")}
+DB[:conn].execute(sql)
+csv = File.read('daily_show_guests.csv')
+id = 0
+CSV.parse(csv, headers: true).each do |row|
+  id += 1
+  sql = <<-SQL
+    INSERT INTO guests (id, year, occupation, show, showgroup, name)
+    VALUES ( ?, ?, ?, ?, ?, ? )
+    SQL
+  DB[:conn].execute(sql, id, row.fields)
+end
+
+binding.pry
+puts 'o'
